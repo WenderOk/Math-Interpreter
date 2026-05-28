670a140013a53effb32e00654de3653c53c7e2bf
WenderOk <wondergeorgy@yandex.ru>
2026-05-28 18:34:08 +0300

    feat: add code for test 4

--- src/modules/Lexer.h
+++ src/modules/Lexer.h
@@ -88,14 +88,22 @@ namespace Interpreter
         {
             Tokens output;
             Tokens stack;
+            auto popAll = [&]() { while(!stack.empty()) 
+            {
+                output.push_back(stack.back());
+                stack.pop_back();
+            }};
             for(const Token &token : tokens)
             {
-                if(token.Type() == TokenType::Number)
-                    output.push_back(token);
-                else
+                if(token.Type() == TokenType::Operator)
+                {
+                    popAll();
                     stack.push_back(token);
+                    continue;
+                }
+                output.push_back(token);
             }
-            std::copy(stack.crbegin(), stack.crend(), std::back_inserter(output));
+            popAll();
             return output; 
         }
     } // namespace Parser
