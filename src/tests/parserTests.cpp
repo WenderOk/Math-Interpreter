4e089e04d5d53c0560040ef0f9202786f419ffc0
WenderOk <wondergeorgy@yandex.ru>
2026-05-28 17:45:26 +0300

    feat: add Should_return_empty_list_when_put_empty_list test

--- src/tests/parserTests.cpp
+++ src/tests/parserTests.cpp
@@ -1,2 +1,23 @@
 #include "Test.h"
+#include "../modules/Lexer.h"
 
+//////////\//////
+// ParserTests //
+/////////\///////
+
+// === EmptyPolicy — для наборов тестов без setup/teardown ===
+struct EmptyPolicy {
+    void setup() {}
+    void teardown() {}
+};
+
+static MereTDD::TestSuiteSetupAndTeardown<EmptyPolicy> gParserSuite("Parser Tests", "Parser");
+
+using namespace Interpreter;
+
+// 1 //
+TEST_SUITE("Should_return_empty_list_when_put_empty_list", "Parser")
+{
+    Tokens tokens = Parser::Parse({});
+    CONFIRM(true, tokens.empty());
+}
