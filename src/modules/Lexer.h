983e3e53bff322de064da9d33611b817b52060e0
WenderOk <wondergeorgy@yandex.ru>
2026-05-28 19:31:18 +0300

    feat: add code for test 7

--- src/modules/Lexer.h
+++ src/modules/Lexer.h
@@ -48,13 +48,36 @@ namespace Interpreter
             if(op == Operator::Mult) content = std::string{Operator::Mult};
             if(op == Operator::Div) content = std::string{Operator::Div};
         }
+
         bool operator==(const Token& t) const
         { return (type == t.type) && (content == t.content); }
+
+        operator Operator() const 
+        {
+            if(type != TokenType::Operator) throw std::logic_error("Should be operator token.");
+            return static_cast<Operator>(content[0]);
+        }
+        
         std::string getContent() const
         { return content; }
         TokenType Type() const
         { return type; }
     };
+
+    inline std::ostream& operator<<(std::ostream& os, const Token& token) 
+    {
+        os << "Token{";
+        switch(token.Type()) {
+            case TokenType::Operator: 
+                os << "Operator:'" << token.getContent() << "'"; break;
+            case TokenType::Number:   
+                os << "Number:" << token.getContent(); break;
+            case TokenType::Unknown:  
+                os << "Unknown:'" << token.getContent() << "'"; break;
+        }
+        os << "}";
+        return os;
+    }
 
     typedef std::vector<Token> Tokens;
 
@@ -84,30 +107,36 @@ namespace Interpreter
 
     namespace Parser 
     {
+        inline int PrecedenceOf(Operator op) 
+        { return (op == Operator::Mult || op == Operator::Div) ? 1 : 0; }
+
         inline Tokens Parse(const Tokens &tokens) 
         {
             Tokens output;
             Tokens stack;
-            auto popAll = [&]() { while(!stack.empty()) 
+            auto popToOutput = [&output, &stack](auto whenToEnd) 
             {
-                output.push_back(stack.back());
-                stack.pop_back();
-            }};
-            for(const Token &token : tokens)
-            {
-                if(token.Type() == TokenType::Operator)
+                while( !stack.empty() && !whenToEnd(stack.back()) )
                 {
-                    popAll();
-                    stack.push_back(token);
+                    output.push_back(stack.back());
+                    stack.pop_back();
+                }
+            };
+
+            for(const Token &current : tokens)
+            {
+                if(current.Type() == TokenType::Operator)
+                {
+                    popToOutput( [&](Operator top){ return PrecedenceOf(top) < PrecedenceOf(current); } );
+                    stack.push_back(current);
                     continue;
                 }
-                output.push_back(token);
+                output.push_back(current);
             }
-            popAll();
+            popToOutput([](auto) { return false; });
             return output; 
         }
 
-        inline int PrecedenceOf(Operator op) 
-        { return (op == Operator::Mult || op == Operator::Div) ? 1 : 0; }
+        
     } // namespace Parser
 }
